4717483b325090b6dc92eccb1f2e88199ecf94ae
MaelleCornec <144256593+MaelleCornec@users.noreply.github.com>
2023-10-20 10:57:26 +0200

    Amélioration du workflow.py

--- workflow.py
+++ workflow.py
@@ -1,151 +1,86 @@
-# ce qu'il faut sortir à la fin
-# je ne sais pas
-# on n'est pas encore arrivé jusque-là
+# des téléchargements ont déjà été faits :
+# - les 6 échantillons d'intérêt (6 fichiers fastq)
+# - le génome de référence (reference.fasta)
+# - les annotations du génome de  référence (reference.gff)
+# - les noms des gènes du génome de référence (on ne les a pas encore !)
+
+# des containers ont été créés :
+# - trim_galore
+# - bowtie
+# - feature_count
 
 echantillons = ['SRR10379721','SRR10379722','SRR10379723','SRR10379724','SRR10379725','SRR10379726']
 
 rule all:
     input:
-        expand("....", echantillon=echantillons)
-
-# téléchargement du génome de référence
-# on peut utiliser le même conteneur que pour le téléchargement des échantillons, non ?
-
-rule downloadingenome:
-    output:
-        "reference/genome.fastq"
-    container:
-        "fastqc:v1"
-    threads:1
-    script:
-        "downloadingen.sh"
-
-# dans downloadingen.sh :
-#$ wget -q -O reference.fasta "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nuccore&id=CP000253.1&rettype=fasta"
-
-# téléchargement des annotations du génome de référence
-# je ne sais pas du tout où trouver ça ?
-# et quelle est la forme de ce fichier d'annotation ?
-
-rule downloadingannot:
-    output:
-        "annotation.txt"
-    script:
-        "downloadingannot.sh"
-
-# dans downloadingannot.sh :
-# $ wget -O reference.gff "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?db=nuccore&→report=gff3&id=CP000253.1"
-
-# téléchargement des noms de gènes du génome de référence et des chemins
-# je ne sais pas du tout où trouver ça ?
-# et quelle est la forme de ce fichier d ?
-
-rule downloadingnoms:
-    output:
-        "noms.txt"
-    script:
-        "downloadingnoms.py"
+        "reference.fasta"
+        "reference.gff"
+        expand("echantillonstests/{echantillon}.fastq", echantillon=echantillons)
 
 # création de l'index sur le génome de référence
 
 rule indexing:
     input:
-        "genome.fastq"
+        "reference.fasta"
     output:
-        "indexGenome ???"
+        "indexation.txt"
     container:
-        "bowtie:v1"
-    script:
-        "indexing.sh"
-
-# dans indexing.sh :
-# $ bowtie-build genome.fastq index
-
-# téléchargement des données 6 fichiers fastq
-# je veux que mes 6 échantillons soient dans un dossier nommé 'echantillons'
-# mais est-ce que c'est vraiment pratique de retélécharger ces fichiers à chaque fois ? Il sont très volumineux...
-# ah, mais snakemake ne devrait pas me les retélécharger s'ils sont déjà là, donc c'est pas grave
-# je continue
-
-rule downloadingechantillons:
-    input:
-        "{echantillon}""
-    output: 
-        "echantillons/{echantillon}.fastq"
-    container:
-        "fastqc:v1"
-    threads:4
-    script: 
-        "downloadingech.sh"
-
-# dans downloadingech.sh (à développer, voir script de Suzanne):
-# $ fasterq-dump --progress {echantillon}
-# $ gzip *.fastq
+        "bowtie"
+    shell: 
+        "bowtie-build genome.fastq indexation.txt"
 
 # Trimming : coupe des morceaux d'échantillons qui sont trop incertains
 # et élimination des échantillons de moins de 25 nucléotides
 
 rule trimming:
     input:
-        "liste des echantillons : echantillonInit/{echantillon}.fastq"
+        "{echantillon}.fastq"
     output:
-        "liste des échantillons moins ceux qui sont trop courts/peu certains : echantillonCoupe/{echantillon}.fastq"
+        "echantillonstests/{echantillon}.fastq"
     container:
-        "trimGalore:v1"
-    script:
-        "trimming.sh"
-
-# dans trimming.sh (à développer):
-# $ trim_galore -q 20 --phred33 --length 25 echantillonInit/{echantillon}.fastq
+        "trim_galore"
+    shell:
+        "trim_galore -q 20 --phred33 --length 25 echantillonstests/{echantillon}.fastq"
 
 # Cartographie des échantillons grâce à l'index fait sur le génôme de référence
 
 rule mapping:
     input:
-        "échantillons coupés précédemment : echantillonCoupe/{echantillon}.fastq"
-        "indexGenome"
+        "{echantillon}.fastq"
+        "indexation.txt"
     output:
-        "échantillons cartographiés : echantillonCarto/{echantillon}.fastq"
+        "carto/{echantillon}.bam"
     container:
-        "bowtie:v1"
-    script:
-        "mapping.sh"
-
-# dans mapping.sh (à développer et à comprendre):
-# $ bowtie -p <#cpus> -S indexGenome <(gunzip -c <GZIPED FASTQ FILE>) | \samtools sort -@ <#CPUS> > <NAME>.bam
-# $ samtools index <NAME>.bam
+        "bowtie"
+    shell:
+        """
+        bowtie -p 4 -S indexation.txt {echantillon}.fastq | \samtools sort -@ 4 carto/{echantillon}.bam
+        samtools index carto/{echantillon}.bam
+        """
 
 # dénombrage des motifs présents sur les échantillons
 # grâce aux annotations du génome de référence
 
 rule counting:
     input:
-        "echantillonCarto/{echantillon}.fastq"
-        "annotation.txt"
+        "carto/{echantillon}.bam"
+        "reference.gff"
     output:
-        "echantillonAnnot/{echantillon}.fastq ???"
+        "compt/counts.txt"
     container:
-        "featureCount:v1"
-    script:
-        "counting.sh"
-
-# dans counting.sh
-# $ featureCounts --extraAttributes Name -t gene -g ID -F GTF -T <#CPUS> -a <GFF> -o counts.txt <BAM FILES>
+        "feature_count"
+    shell:
+        "featureCounts --extraAttributes Name -t gene -g ID -F GTF -T 4 -a reference.gff -o counts.txt {echantillon}.bam
 
 # analyse des échantillons
 # en connaissant le nom des gènes
 
 rule analysis:
     input:
-        "echantillonAnnot/{echantillon}.fastq ???"
-        "nom.txt"
+        "compt/counts.txt"
     output:
         "je ne sais vraiment plus"
     container:
-        "deSeq2:v1"
+        "de_seq2"
     script:
         "analysis.py"
-
-# en vrai, je pourrais probablement mettre tous les scripts directement dans les étapes
-# en utilisant : 
-# shell: " ... "
