47402e62e9976178f7431ab01cd220005f489f10
suzannegtx <suzanne.guilteaux@gmail.com>
2023-11-02 22:17:45 +0000

    à tester demain matin avec plus grosse VM

--- containers/Bowtie/bowtie.sh
+++ containers/Bowtie/bowtie.sh
@@ -1,9 +1,11 @@
 # Déplacement du fichier fasta du génome de référence dans le dossier avec le Dockerfile et l'exécutable
-mv /home/ubuntu/Reprohackathon2023/reference.fasta /home/ubuntu/Reprohackathon2023/containers/Bowtie/
+# cp /home/ubuntu/Reprohackathon2023/reference.fasta /home/ubuntu/Reprohackathon2023/containers/Bowtie/
 
 # Construction de l'image Docker
-docker build -t bowtie-0.12.7-image .
+docker build -t bowtie:0.12.7 /home/ubuntu/Reprohackathon2023/containers/Bowtie
+
 # Exécution de l'image
-docker run -it bowtie-0.12.7-image
+docker run -it bowtie:0.12.7
+
 # Indexation du génome de référence 
-bowtie-build reference.fasta genome_ref_index
+#bowtie-build reference.fasta genome_ref_index
